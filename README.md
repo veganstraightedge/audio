5d93f275eae0a08810e4a1782bc60211aa34e0b1
Shane Becker <veganstraightedge@gmail.com>
2014-06-09 14:28:44 -0700

    put todo in readme

---
+++ README.md
@@ -0,0 +1,10 @@
+# band urls to redirect
+
+/bands/3/bombs-and-beating-hearts
+/bands/8/bramble
+/bands/1/fuck-yes
+/bands/6/harpers-ferry-arsenal
+/bands/2/james-miska
+/bands/7/rise-over-run
+/bands/4/the-mooks
+/bands/5/yunavi-(formerly-trebuchet)
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,24 @@
 module ApplicationHelper
+  
+  # previously in /lib/url.rb
+  # module Url
+  def sanatized_album_seo_path(album)
+    album_seo_path(album, sanatize_for_url(album.name), sanatize_for_url(album.band.name))
+  end
+
+  def sanatized_band_seo_path(band)
+    band_seo_path(band, sanatize_for_url(band.name))
+  end
+
+  def sanatized_song_seo_path(song)
+    song_seo_path(song,
+                  sanatize_for_url(song.pretty_name),
+                  sanatize_for_url(song.album.band.name),
+                  sanatize_for_url(song.album.name))
+  end
+
+  def sanatize_for_url(thing)
+    thing.downcase.gsub(/&/, "and").gsub(/ /, "-").gsub(/,|\.|'|\/|\\|!|:|\?|#|\(|\)|\[|\]/, "").gsub(/--/, "-")
+  end
+  
 end
