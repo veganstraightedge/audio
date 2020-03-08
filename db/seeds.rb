f68406c3612c18ca85969ebca249f093723cd516
Shane Becker <veganstraightedge@gmail.com>
2020-03-08 01:11:19 -0800

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,359 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+{
+  "Aiden and Lucas" => {
+    :quotes => {
+      "Filler" => %q(What happened to you? You're not the same. Something in your head made a violent change.)
+    },
+
+    "Candy Pops" => [
+      "Studyhall Remix (Spunkmeyer cover)",
+      "Filler",
+      "02 Legos",
+      "06 N8 on the Go"
+    ]
+  },
+
+  "Bombs and Beating Hearts" => {
+    :quotes => {
+      "01 Love Is Freedom" => %q(And we'll need love like this bomb inside of me. It can kill you or it can set you free. And we'll need violence like this beating heart you see. It can kill you or it can set you free.),
+      "01 Love Is Freedom" => %q(How can you be afraid if your life never even starts? We're the deadliest things in the world, fucking bombs and beating hearts.)
+    },
+
+    "From Dumpsters Rise" => [
+      "01 Love Is Freedom",
+      "02 U.S. And Them",
+      "03 Anatomy Of An Activist",
+      "04 Folsom Prison (Featuring Tom Frampton)",
+      "05 Normandie",
+      "06 The Pyrate Song",
+      "07 Citation Song",
+      "08 A Girl Named Jerk",
+      "09 Peter Pan Syndrome",
+      "10 Missionary",
+      "11 So Many",
+      "12 Luftwaffe",
+      "13 Shiny Happy People Holding Bombs",
+      "14 Some Call It Hate"
+    ],
+
+    "Oh shit, an acoustic split!" => [
+      "10 Anarchy Baseball (Hold The Umpire)",
+      "11 Halfway Hill",
+      "12 Taking Back Control",
+      "13 Stiff Joints and Gasoline",
+      "14 Shiny Happy People Playing Dub",
+      "15 Freebasers Do It Better (Dubbed Cover)",
+      "16 Love Is Freedom",
+      "17 O's -> <3's",
+      "18 The Pyrate Song"
+    ]
+  },
+
+  "Bramble" => {
+    :quotes => {
+      "02 The Bridge" => %q(And I'd like to sing you a glorious song... As we cross the bridge, from "What if we're right?"s, to "What could be wrong?"s)
+    },
+
+    "H.A.G.S." => [
+      "01 Fruit Of The Moon",
+      "02 The Bridge",
+      "03 Trains X",
+      "04 Colors",
+      "05 Light",
+      "06 Seah"
+    ],
+
+    "Campfire EP" => [
+      "01 So It Goes",
+      "02 Dirty Bliss",
+      "03 Birds"
+    ]
+  },
+
+  "Fuck, Yes!!!!" => {
+    :quotes => {
+      "02 Fighting for Our Lives" => %q(When we fight, we're fighting for our lives. And you can bet that's a threat and we're not gonna stop until every last law is broken and every fucking cop is dead.)
+    },
+
+    "Misc" => [
+      "01 A Toast to the Fallen",
+      "02 Fighting for Our Lives",
+      "03 Awfully Big Adventure",
+      "04 Drugs and Daydreams",
+      "05 Fifty Six And A Half Inches Of Freedom",
+      "06 Recipe for Disaster",
+      "07 Let it Go"
+    ],
+
+    "Demo" => [
+      "01 Fifty Six And A Half Inches Of Freedom",
+      "02 Fighting For Our Lives",
+      "03 An Awfully Big Adventure (Live)",
+      "04 [Untitled] (Live) / No Children (by The Mountain Goats)"
+    ]
+  },
+
+  "Harper's Ferry Arsenal" => {
+    :quotes => {
+      "06 Same Shit, Different Stop" => %q(Alienated, we flock to the convenience of alienation and further cycle of exploitation and oppression, but I hope and I dream of a world where we rely on ourselves for all we need.)
+    },
+
+    "Demo 2004" => [
+      "01 Who Controls the Past",
+      "02 Consenting Adults (at V. With)",
+      "03 Rape Culture 101",
+      "04 Desert Storm, NYC",
+      "05 317",
+      "06 Same Shit, Different Stop"
+    ]
+  },
+
+  "James Miska" => {
+    :quotes => {
+      "02 Trebuchet" => %q(I see no reason we should all live and die alone.)
+    },
+
+    "You'll Think What You Want to Think (And Do What You Want to Do)" => [
+      "01 Castaway",
+      "02 Trebuchet",
+      "03 Valentine's Day",
+      "04 Unoriginal",
+      "05 Electric Blankets",
+      "07 Perfectionist",
+      "08 For the Spite",
+      "10 Rods and Cones",
+      "11 Melea"
+    ],
+
+    "Live at Red Light Books 5.12.2007" => [
+      "01 I Am Coming Home"
+    ],
+
+    "Silverback Gorilla" => [
+      "01 Mischief",
+      "02 Grace",
+      "03 Chin Up",
+      "04 White Tourist",
+      "05 Quetzalcoatl",
+      "06 Uncooked",
+      "07 Our Lookout",
+      "08 Ellemetchef",
+      "09 Rods and Cones",
+      "10 Perfectionist",
+      "11 Lined Paper",
+      "12 Spainish",
+      "13 Street Car",
+      "14 Trebuchet",
+      "15 Time (Blood On the Reeds)",
+      "16 Not Perfect",
+      "17 Atonement"
+    ],
+
+    "Time Alone" => [
+      "01 Never (going away)",
+      "02 Was (valentine's day)",
+      "03 There (trains)",
+      "04 A (electric blankets)",
+      "05 Time (unoriginal)",
+      "06 When (for the spite)",
+      "07 The (Melea)",
+      "08 World (door unlocked)",
+      "09 Didn't (sailing)",
+      "10 Show (castaway)",
+      "11 Me (victory parade)",
+      "12 Something (apple pie)",
+      "13 New (time alone)",
+      "14 Every (coming home)",
+      "15 Day (night's black skirt)"
+    ],
+
+    "Live at Boing! 9.9.2005" => [
+      "01 Raise Your Glasses",
+      "02 Bloomington Backwoods",
+      "03 47 Times",
+      "04 Of Bicycles And Butterknives (Graveyards And A Grand Vision Of Things)",
+      "05 The Airplane Song",
+      "06 Still Sleeps Tonight",
+      "07 The Clap A Long Song",
+      "08 The Intersection",
+      "09 Seriously, Who Gives A Fuck? You're Wasting Your Life Thinking About It."
+    ],
+
+    "Saol, Ceol" => [
+      "01 The Airplane Song",
+      "02 A Plague O'Both Our Houses",
+      "03 The Intersection",
+      "04 Anthem Of Positivity (For The Traveling, Adventure-Seeking, And Otherwise Nomadically-Minded, Kids)",
+      "05 47 Times",
+      "06 Bloomington Backwoods",
+      "07 Pendleton Revisited (Radio Silence)",
+      "08 Still Sleeps Tonight",
+      "10 Of Bicycles And Butterknives (Graveyards And A Grand Vision Of Things)",
+      "11 Raise Your Glasses"
+    ],
+
+    "Live at Red Light Books 11.23.2007" => [
+      "01 The Newest One That He Doesn't Remember",
+      "02 Talking Talking Talking 1",
+      "03 Perfectionist",
+      "04 Talking Talking Talking 2",
+      "05 Unoriginal",
+      "06 Talking Talking Talking 3",
+      "07 Cast Away",
+      "08 Talking Talking Talking 4",
+      "09 Tonight We're Gonna Give It 35%",
+      "10 Talking Talking Talking 5",
+      "11 Anthem of Positivity",
+      "12 Talking Talking Talking 6",
+      "13 For the Spite",
+      "14 Talking Talking Talking 7",
+      "15 Electric Blankets",
+      "16 Talking Talking Talking 8",
+      "17 Trebuchet",
+      "18 Talking Talking Talking 9",
+      "19 Melea",
+      "20 Talking Talking Talking 10",
+      "21 Rods and Cones",
+      "22 Talking Talking Talking 11",
+      "23 The Land of 1000 Naps"
+    ],
+
+    "Yep" => [
+      "01 Grace",
+      "02 Trains",
+      "03 Unlocked"
+    ]
+  },
+
+  "Rise Over Run" => {
+    :quotes => {
+      "19 Prauge Was A Good Start" => %q(I want peace. We all want peace. But I'd rather be free.)
+    },
+
+    "Brycc House Benefit" => [
+      "05 Gladiator"
+    ],
+
+    "Art, Friends, and Revolution (Not Necessarily in That Order)" => [
+      "01 Introduction",
+      "02 Choice Of A New Generation Intro",
+      "03 Choice Of A New Generation",
+      "04 Son Of A Senator Vs. Son Of A President Intro",
+      "05 Son Of A Senator Vs. Son Of A President",
+      "06 Columbus Day Intro",
+      "07 Columbus Day",
+      "08 S.U.V. Intro",
+      "09 Public Notice / S.U.V.",
+      "10 Bert And Ernie Are Dead Intro",
+      "11 Bert And Ernie Are Dead",
+      "12 Food For Oil Intro",
+      "13 Food For Oil",
+      "14 I Have A Hairy Ass And I'm Proud Of It Intro",
+      "15 I Have A Hairy Ass And I'm Proud Of It",
+      "16 Cut That Mullet Intro",
+      "17 Cut That Mullet",
+      "18 Prauge Was A Good Start Intro",
+      "19 Prauge Was A Good Start",
+      "20 Willie Wonka Intro",
+      "21 Willie Wonka",
+      "22 Honk"
+    ]
+  },
+
+  "Ten Times Over" => {
+    :quotes => {
+      "05 Open Letter to the FBI" => %q(This time we're not moving a fucking inch.)
+    },
+
+    "Demo" => [
+      "01 Suffering",
+      "02 Worship",
+      "03 People",
+      "04 Licker",
+      "05 Open Letter to the FBI"
+    ]
+  },
+
+  "The Mooks" => {
+    :quotes => {
+      "06 Sweet Corn" => %q(Ten yard fight scoring in the end zone, while I'm stuck in the friend zone. You say you wanna be best friends. Fuck that. I wanna be your boyfriend.)
+    },
+
+    "Like You Like You" => [
+      "01 The Snuggle Continues"
+    ],
+
+    "The Snuggle Sessions" => [
+      "01 Sugarscoot",
+      "02 Gutter Fever",
+      "03 Mooks Of Today",
+      "04 We Wanna Be The Steinways",
+      "05 I Wish The Steinways Would Reunite (And Cover This Song)",
+      "06 Sweet Corn",
+      "07 Mr. Dink Is A Pedophile",
+      "08 Jano Goes To College"
+    ]
+  },
+
+  "Yunavi (formerly Trebuchet)" => {
+    :quotes => {
+      "04 Defeatism Is For Losers" => %q(We don't care about the fire that's coming, because we're already candles burning.)
+    },
+
+    "The Wilderness" => [
+      "01 Opening Hymn",
+      "02 7 Days Alone In The Woods",
+      "03 Marrow House",
+      "04 Defeatism Is For Losers",
+      "05 Ze Germans",
+      "06 Ladders",
+      "07 Home",
+      "08 Sex",
+      "09 Post-Modernism",
+      "10 Candelabrum",
+      "11 Land And Sea",
+      "12 Take Time"
+    ],
+
+    "Demo (as Trebuchet)" => [
+      "01 32",
+      "02 Postmodernism",
+      "03 7 Days Alone in the Woods",
+      "04 Defeatism is for Losers"
+    ],
+
+    "Live on KRCL (as Trebuchet)" => [
+      "01 Ladders",
+      "02 Defeatism is for Losers",
+      "03 7 Days Alone in the Woods",
+      "04 PostModernism"
+    ]
+  }
+}.each do |band, albums|
+  b = Band.create!(:name => band)
+  puts "#{b.name}"
+
+  albums.each do |album, songs|
+    unless album.to_s == "quotes"
+      a = Album.create!(:name => album, :band_id => b.id)
+      puts "  #{a.name}"
+
+      songs.each do |song|
+        s = Song.create!(:name => song, :album_id => a.id)
+        puts "    " + s.name
+      end
+
+      puts
+    end
+  end
+
+  albums[:quotes].each do |song, excerpt|
+    s = Song.find_by_name(song)
+
+    q = Quote.create!(:song_id => s.id, :band_id => b.id, :excerpt => excerpt)
+
+    puts "  QUOTE: " + q.excerpt
+    puts
+  end
+
+  puts
+end
